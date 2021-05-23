c4cf104d7380e50c8a678e3bb781359a6ebf1330
Buğra Yüksel <P1975@DETAY.local>
2021-05-23 20:57:47 +0300

    personal website updatesv2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var typed = new Typed('#typed',{
                    strings:["SAP Fiori Consultant","Software Developer", "Science Lover","Space Addict"],
                    backSpeed: 40,
-                   typeSpeed: 40,
+                   typeSpeed: 80,
                    loop: true,
                    shuffle: false
                  });
